bdff64a8837eda164a1538975c00b6141ffd9091
ChalakaK94 <karunarathne.cp@gmail.com>
2023-11-30 22:12:12 +0530

    implement map component and style

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,27 @@
+
+import {useMediaQuery} from "@material-ui/core";
+import  GoogleMapReact from 'google-map-react'
+import useStyle from './styles'
+
 export function Map() {
+    const classes = useStyle();
+    const isMobile = useMediaQuery('{min-width:600px}')
+
+    const cordi = {lat:0, lng:0}
     return (
-        <>
-            <div>Map</div>
-        </>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key:'AIzaSyAKE7d10sGug7ja4M8y0IL4PAzqFWy36G4'}}
+                defaultCenter={cordi}
+                center={cordi}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''} />
+
+
+
+        </div>
     )
 }
